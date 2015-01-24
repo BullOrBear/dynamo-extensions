12f6a5fba6ca93e32d45bded062ee543dd567a36
Aaron Signorelli <aaron.signorelli@bullorbear.com>
2015-01-24 01:34:01 +0000

    adding thred id

--- src/main/java/com/bullorbear/dynamodb/extensions/datastore/Datastore.java
+++ src/main/java/com/bullorbear/dynamodb/extensions/datastore/Datastore.java
@@ -40,7 +40,7 @@ public class Datastore {
 
   public <T extends DatastoreObject> T put(T object) {
     String txId = transaction == null? "NO-TX" : transaction.getTransactionId();
-    System.out.println(Thread.currentThread().getId() + " > Putting object into transaction: " + txId + " " + object);
+    System.out.println(this + " > Putting object into transaction: " + txId + " " + object);
     if (object != null && Task.class.isAssignableFrom(object.getClass())) {
       // Intercept tasks here
       ((TransactionalExecutor) executor).queueTask((Task) object);
@@ -91,6 +91,8 @@ public class Datastore {
       });
     }
     
+    System.out.println(this + " > Transaction begun: " + this.transaction.getTransactionId());
+
     return this.transaction;
   }
 
